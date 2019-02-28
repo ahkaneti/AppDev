93d3736426e30b353a89a1a9f3cf44f43a53ef99
Bradley Ramos <bradleymramos@gmail.com>
2019-02-28 14:16:20 -0600

    Set up registration sender

--- tagalong/src/components/CreatePage.js
+++ tagalong/src/components/CreatePage.js
@@ -10,27 +10,83 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Image, TouchableOpacity} from 'react-native';
 
-
 type Props = {};
 class CreatePage extends Component{
-  RegisterFunction(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      phone_number: '',
+      password: '',
+    };
+  }
+  RegisterFunction() {
+    return fetch('http://localhost:8000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          email: this.state.email,
+          phone_number: this.state.phone_number,
+          password: this.state.password })
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+        console.log(responseData)
+    });
+  }
+  GoToAuth() {
     this.props.navigation.navigate('AuthentificationPage');
-  };
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.logo}>Create Account</Text>
         <Text style={styles.headers}>First Name</Text>
-        <TextInput style={styles.entry}/>
+        <TextInput
+          style={styles.entry}
+          onSubmitEditing={(first_name) => this.setState({first_name})}
+        />
         <Text style={styles.headers}>Last Name</Text>
-        <TextInput style={styles.entry}/>
+        <TextInput
+          style={styles.entry}
+          onSubmitEditing={(last_name) => this.setState({last_name})}
+        />
         <Text style={styles.headers}>Phone Number</Text>
-        <TextInput style={styles.entry}/>
+        <TextInput
+          style={styles.entry}
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          keyboardType={'numeric'}
+          onSubmitEditing={(phone_number) => this.setState({phone_number})}
+        />
         <Text style={styles.headers}>Email</Text>
-        <TextInput style={styles.entry}/>
+        <TextInput
+          style={styles.entry}
+          autoCapitalize={'none'}
+          keyboardType={'email-address'}
+          autoCorrect={false}
+          onSubmitEditing={(email) => this.setState({email})}
+        />
         <Text style={styles.headers}>Password</Text>
-        <TextInput style={styles.entry}/>
-        <TouchableOpacity style={styles.login_bttn} onPress = {()=>this.RegisterFunction()}><Text style={styles.bttn_text}>Create</Text></TouchableOpacity>
+        <TextInput
+          style={styles.entry}
+          autoCapitalize={'none'}
+          onSubmitEditing={(password) => this.setState({password})}
+        />
+        <TouchableOpacity style={styles.login_bttn}
+          onPress = {() => {
+            this.RegisterFunction()
+            this.GoToAuth()
+          }}>
+          <Text style={styles.bttn_text}>Create</Text>
+        </TouchableOpacity>
       </View>
     );
   }
