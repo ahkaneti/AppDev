b38f5513fc73ecabdbbe6fe4882fcf0210f78fa3
Bradley Ramos <bradleymramos@gmail.com>
2019-03-02 05:53:03 -0600

    Profile start

--- tagalong/src/components/ProfilePage.js
+++ tagalong/src/components/ProfilePage.js
@@ -9,11 +9,31 @@ class ProfilePage extends Component{
   constructor(props){
     super(props);
     this.state={
-      nameText: "",
-      emailText: "",
-      phoneText: "",
+      firstnameText: '',
+      lastnameText: '',
+      emailText: '',
+      phoneText: '',
     }};
 
+  componentDidMount() {
+    console.log(this.state);
+    return fetch(`https://bradleyramos-login-boiler-plate-2.glitch.me/profile?secret_token=${global.token}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        }).then((response) => response.json())
+        .then((responseJson)=> {
+            this.setState({firstnameText: responseJson.first_name});
+            this.setState({lastnameText: responseJson.last_name})
+            this.setState({emailText: responseJson.email})
+            this.setState({phoneText: responseJson.phone_number})
+        })
+        .catch((error)=> {
+          console.error(error);
+        });
+  }
 
   SignOutFunction(){
     this.props.navigation.navigate('CreateLoginPage');
@@ -25,7 +45,7 @@ class ProfilePage extends Component{
       <View style={styles.container}>
         <TouchableOpacity style={styles.edit}><Image style={{width: 40, height: 40}} source={require('../../images/gear.png')}/></TouchableOpacity>
         <Image style={styles.profile} source={require('../../images/Profileimg.png')}/>
-        <Text style={styles.name}>Bradly Ramos</Text>
+        <Text style={styles.name}>Bradley Ramos</Text>
         <Text style={styles.email}>bradleyramos@yahoo.com</Text>
         <Text style={styles.phone}>224-545-8491</Text>
         <TouchableOpacity onPress = {()=>this.SignOutFunction()} style={styles.signOut}><Text style={styles.signOutText}>Sign Out</Text></TouchableOpacity>
